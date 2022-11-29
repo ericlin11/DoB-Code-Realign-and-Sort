561e7ceefbce543f264e9ba33b38114f3c60e827
Eric Lin <eric.lin206@gmail.com>
2022-11-29 15:48:15 -0500

    added sorting (ascending order)

--- main.py
+++ main.py
@@ -81,24 +81,85 @@ for x in range(2, ws.max_row+1):
                     print("_______________")
 
 
-# #Sorting the spreadsheet by Section Number
-# print("SORTING ...")
-# for x in range(2, ws.max_row+1):
-#     if ws.cell(x, column=1).value and ws.cell(x, column=3).value is not None:
-#         print("same")
-#         print("_______________")
-#     elif ws.cell(x, column=1).value is not None:
-#         nyc = ws.cell(x, column=1).value.split(" ", 1)
-#         nycnumber = nyc[0]
-#         print(nycnumber)
-#         print("_______________")
-#     elif ws.cell(x, column=3).value is not None:
-#         icc = ws.cell(x, column=3).value.split(" ", 1)
-#         iccnumber = icc[0]
-#         print(iccnumber)
-#         print("_______________")
-#
-# # sorted(inputlist, key=lambda v: [int(i) for i in v.split('.')])
+#Sorting the spreadsheet by Section Number
 
-wb.save('new.xlsx')
+
+print("SORTING ...")
+inputlist = []
+for x in range(2, ws.max_row+1):
+    if ws.cell(x, column=1).value and ws.cell(x, column=3).value is not None:
+        nyc = ws.cell(x, column=1).value.split(" ", 1)
+        icc = ws.cell(x, column=3).value.split(" ", 1)
+        nycnumber = nyc[0]
+        iccnumber = icc[0]
+        if nycnumber == iccnumber:
+            sectionnumber = ws.cell(x, column=1).value.split(" ", 1)
+            inputlist.append({
+                        "sectionnumber": sectionnumber[0],
+                        "nycsection": ws.cell(x, column=1).value,
+                        "nyctext": ws.cell(x, column=2).value,
+                        "iccsection": ws.cell(x, column=3).value,
+                        "icctext": ws.cell(x, column=4).value,
+                    })
+        else: print('error' + ws.cell(x, column=1).value + " - " + ws.cell(x, column=3).value)
+    elif ws.cell(x, column=1).value is not None:
+        nyc = ws.cell(x, column=1).value.split(" ", 1)
+        nycnumber = nyc[0]
+        inputlist.append({
+            "sectionnumber": nycnumber,
+            "nycsection": ws.cell(x, column=1).value,
+            "nyctext": ws.cell(x, column=2).value,
+            "iccsection": ws.cell(x, column=3).value,
+            "icctext": ws.cell(x, column=4).value,
+        })
+    elif ws.cell(x, column=3).value is not None:
+        icc = ws.cell(x, column=3).value.split(" ", 1)
+        iccnumber = icc[0]
+        inputlist.append({
+            "sectionnumber": iccnumber,
+            "nycsection": ws.cell(x, column=1).value,
+            "nyctext": ws.cell(x, column=2).value,
+            "iccsection": ws.cell(x, column=3).value,
+            "icctext": ws.cell(x, column=4).value,
+        })
+
+sortedlist = sorted(inputlist, key=lambda d: d['sectionnumber'])
+
+startingrow = 2
+
+for x in sortedlist:
+    ws.cell(startingrow, column=1).value = x["nycsection"]
+    ws.cell(startingrow, column=2).value = x["nyctext"]
+    ws.cell(startingrow, column=3).value = x["iccsection"]
+    ws.cell(startingrow, column=4).value = x["icctext"]
+    print(str(ws.cell(startingrow, column=1)) + "\n" +
+          str(x["sectionnumber"]) + ": \n" +
+          str(x["nycsection"]) + " - " + str(x["nyctext"]) + "\n " +
+          str(x["iccsection"]) + " - " + str(x["icctext"]) + "\n________")
+    startingrow += 1
+
+
+
+wb.save('sorted.xlsx')
 print("Done")
+
+# Another way of sorting and realigning.
+# 1. Check if column 1 have value. If it does, get their nycsection, nycnumber, nyctitle from column 1, and nyctext from column 2.
+# 2. Check the next row of column 1 for values. If it has value. Get their nycnumber. compare it to the existing nycnumber in 'codes'.
+# 3. SORT : If smaller, go in front. If bigger, go in back.
+# 4. After, check if column 2 have values. Calculate the iccnumber and compare it to nycnumber in 'codes'.
+# 5. If iccnumber and iccnumber matches. Add the values iccsection, iccnumber, icctitle, and icctext. If not, add new 'codes'.
+# 6. Paste each 'codes' into excel.
+# "codes" : [
+#     {
+#         "id": "",
+#         "nycsection": "",
+#         "nycnumber": "",
+#         "nyctitle": "",
+#         "nyctext": "",
+#         "iccsection": "",
+#         "iccnumber": "",
+#         "icctitle" : "",
+#         "icctext": "",
+#     }
+# ]
